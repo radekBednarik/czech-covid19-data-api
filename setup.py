e96815b20610c8c8a264add7694512dcb5edaf1d
radekBednarik <radek.bednarik@tesena.com>
2020-10-16 16:01:18 +0200

    moved setup.py and fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r") as file:
 
 setup(
     name="czech-covid19-data-api",
-    version="1.0.1",
+    version="1.0.2",
     author="Radek 'bednaJedna' Bednarik",
     packages=find_packages(),
     install_requires=["requests"],
